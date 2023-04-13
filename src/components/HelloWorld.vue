9b50cbfa012a13854dcd76a95d760c874ee6bf06
Ismaelolazo <129405974+Ismaelolazo@users.noreply.github.com>
2023-04-13 12:06:05 -0400

    Isma 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,32 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      Actualmente Banco Unión S.A. cuenta con cuatro filiales (Valores Unión, SAFI Unión, Univida y Unibienes),<br>
-      y se compone de una extensa red de oficinas y cajeros automáticos en todo el país.
-
-El objetivo rector de Banco Unión S.A. se enfoca en crecer y consolidar su posición en los primeros lugares<br>
-del Sistema Financiero Nacional, generando los mejores ratios de rentabilidad al Banco y manteniendo un permanente<br>
-compromiso con el desarrollo del país a través de servicios financieros integrales, eficientes y modernos, ofreciendo<br>
-soluciones ágiles y sobre todo calidad y atención de excelencia.
+      Actualmente Banco Unión S.A. cuenta con cuatro filiales (Valores Unión,
+      SAFI Unión, Univida y Unibienes),<br />
+      y se compone de una extensa red de oficinas y cajeros automáticos en todo
+      el país. El objetivo rector de Banco Unión S.A. se enfoca en crecer y
+      consolidar su posición en los primeros lugares<br />
+      del Sistema Financiero Nacional, generando los mejores ratios de
+      rentabilidad al Banco y manteniendo un permanente<br />
+      compromiso con el desarrollo del país a través de servicios financieros
+      integrales, eficientes y modernos, ofreciendo<br />
+      soluciones ágiles y sobre todo calidad y atención de excelencia.
     </p>
+    <img
+      alt="Vue logo"
+      src="https://cd1.eju.tv/wp-content/uploads/2018/10/5bd300fd89555.jpg"
+      height="80"
+    />
+    <img
+      alt="Vue logo"
+      src="https://cd1.eju.tv/wp-content/uploads/2017/10/59d7a68e58fd0.jpg"
+      height="80"
+    />
+    <img
+      alt="Vue logo"
+      src="https://4.bp.blogspot.com/-gGLBIXTlmtU/WwRVkRrgDMI/AAAAAAAAIDY/-0Co20uAL7kOXCX8TzoVVYw_oro6Td28ACLcBGAs/s1600/banco_union_desfalco.jpg"
+      height="80"
+    />
     <h3>Redes sociales</h3>
     <ul>
       <li>
